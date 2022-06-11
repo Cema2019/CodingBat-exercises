9a03f9fbd66dd10ebc0f156a98d1874164f7966b
Carlos M <53409854+Cema2019@users.noreply.github.com>
2022-06-10 23:35:16 -0600

    Update and rename Array-2 > shiftLeft to Array-2/shiftLeft.java

--- Array-2/shiftLeft.java
+++ Array-2/shiftLeft.java
@@ -1,8 +1,8 @@
-Return an array that is "left shifted" by one -- so {6, 2, 5, 3} returns {2, 5, 3, 6}. You may modify and return the given array, or return a new array.
+/*Return an array that is "left shifted" by one -- so {6, 2, 5, 3} returns {2, 5, 3, 6}. You may modify and return the given array, or return a new array.
 
 shiftLeft([6, 2, 5, 3]) → [2, 5, 3, 6]
 shiftLeft([1, 2]) → [2, 1]
-shiftLeft([1]) → [1]
+shiftLeft([1]) → [1]*/
 
 
 public int[] shiftLeft(int[] nums) {
