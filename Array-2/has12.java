5c038a7291f6736f1fad310b27e94ca52c488566
Carlos M <53409854+Cema2019@users.noreply.github.com>
2022-06-10 23:32:11 -0600

    Update and rename Array-2 > has12 to Array-2/has12.java

--- Array-2/has12.java
+++ Array-2/has12.java
@@ -1,8 +1,8 @@
-Given an array of ints, return true if there is a 1 in the array with a 2 somewhere later in the array.
+/*Given an array of ints, return true if there is a 1 in the array with a 2 somewhere later in the array.
 
 has12([1, 3, 2]) → true
 has12([3, 1, 2]) → true
-has12([3, 1, 4, 5, 2]) → true
+has12([3, 1, 4, 5, 2]) → true*/
 
 
 public boolean has12(int[] nums) {
